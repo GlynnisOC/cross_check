ec42c5d41ef8c0b769888a19e5720ec0341bdc40
Ryan Miller <ryanmillergm@gmail.com>
2019-04-02 17:42:37 -0600

    First 3 methods of iteration 2 done

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,29 @@
 class Game
 
   def initialize(game_stats, game_teams_stats, team_stats)
-    @game_stats= nil
-    @game_teams_stats = nil
-    @team_stats = nil
+    @game_stats= game_stats
+    @game_teams_stats = game_teams_stats
+    @team_stats = team_stats
   end
 
   def highest_total_score
-    @game_stats
+    game = @game_stats.max_by do |game|
+      game[:home_goals] + game[:away_goals]
+    end
+    game[:home_goals] + game[:away_goals]
+  end
+
+  def lowest_total_score
+    game = @game_stats.min_by do |game|
+      game[:home_goals] + game[:away_goals]
+    end
+    game[:home_goals] + game[:away_goals]
+  end
+
+  def biggest_blowout
+    game = @game_stats.max_by do |game|
+      game[:home_goals] - game[:away_goals]
+    end
+    game[:home_goals] - game[:away_goals]
   end
 end
