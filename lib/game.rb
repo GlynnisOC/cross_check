1d9cec94c6c205f68c0a35385a66e186281e3732
Ryan Miller <ryanmillergm@gmail.com>
2019-04-03 10:48:57 -0600

    Add 3 game methods

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,11 @@
 class Game
+  attr_reader :total_goals
 
   def initialize(game_stats, game_teams_stats, team_stats)
     @game_stats= game_stats
     @game_teams_stats = game_teams_stats
     @team_stats = team_stats
+    @total_goals = 0
   end
 
   def highest_total_score
@@ -42,4 +44,49 @@ class Game
     end
     ((counter.to_f / @game_stats.count) * 100).round(2)
   end
+
+  def count_of_games_by_season
+    total_games_by_season = {}
+    @game_stats.each do |game|
+      total_games_by_season[game[:season]] = 0 if !total_games_by_season[game[:season]]
+        total_games_by_season[game[:season]] += 1
+    end
+    total_games_by_season
+  end
+
+  def average_goals_per_game
+    total_games = 0
+    @total_goals = 0
+
+    @game_stats.each do |game|
+      total_games += 1
+      @total_goals += game[:away_goals] + game[:home_goals]
+    end
+    @total_goals
+    (@total_goals / total_games.to_f).round(3)
+  end
+
+  def average_goals_by_season
+
+    total_goals_per_season = {}
+    @game_stats.each do |game|
+      total_goals_per_season[game[:season]] = 0 if !total_goals_per_season[game[:season]]
+        total_goals_per_season[game[:season]] += game[:away_goals] + game[:home_goals]
+    end
+    total_goals_per_season.each_with_index do |(season, goals), index|
+
+      total_goals_per_season[season] = (goals.to_f / count_of_games_by_season.values[index]).round(3)
+    end
+
+    total_goals_per_season
+
+  end
+# goal per season hash {20122013=>69, 20132014=>29, 20142015=>29,
+  # 20162017=>30, 20152016=>5}
 end
+
+#Create method for # of games in a season
+#would have to be able to pass in specific season
+
+#Create method for # of goals in a season
+#would have to be able to pass in specific season
