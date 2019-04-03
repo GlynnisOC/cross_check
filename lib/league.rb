a681621c7b5e6d0b31d1ded5e989d03d05516707
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-04-03 09:02:57 -0600

    League setup and passing first test

--- lib/league.rb
+++ lib/league.rb
@@ -1,8 +1,8 @@
 class League
 
   def initialize(game_stats, game_teams_stats, team_stats)
-    @game_stats= nil
-    @game_teams_stats = nil
-    @team_stats = nil
+    @game_stats= game_stats
+    @game_teams_stats = game_teams_stats
+    @team_stats = team_stats
   end
-end 
+end
