7738d3c48d3bc03478090f736ed0844712a25082
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-04-04 14:28:17 -0600

    Add the best and worst offense method

--- lib/league.rb
+++ lib/league.rb
@@ -9,4 +9,37 @@ class League
   def count_of_teams
     @team_stats.count
   end
+
+  def best_offense
+    result = {}
+    @game_teams_stats.each do |game|
+      id = game[:team_id]
+      result[id] = {games_played: 0, goals: 0} unless result[id]
+      result[id][:games_played]+=1
+      result[id][:goals] += game[:goals]
+    end
+    team_id = result.max_by do |team_id, stats|
+      (stats[:goals]/stats[:games_played].to_f).round(2)
+    end.first
+    @team_stats.find do |team|
+      team[:team_id] == team_id
+    end[:teamname]
+  end
+
+  # require 'pry';binding.pry
+  def worst_offense
+    result = {}
+    @game_teams_stats.each do |game|
+      id = game[:team_id]
+      result[id] = {games_played: 0, goals: 0} unless result[id]
+      result[id][:games_played]+=1
+      result[id][:goals] += game[:goals]
+    end
+    team_id = result.min_by do |team_id, stats|
+      (stats[:goals]/stats[:games_played].to_f).round(2)
+    end.first
+    @team_stats.find do |team|
+      team[:team_id] == team_id
+    end[:teamname]
+  end
 end
--- test/league_test.rb
+++ test/league_test.rb
@@ -7,9 +7,9 @@ require './lib/stat_tracker'
 class LeagueTest < Minitest::Test
 
   def setup
-    game_path = './data/dummy_files/game_dummy.csv'
-    team_path = './data/dummy_files/team_dummy.csv'
-    game_teams_path = './data/dummy_files/game_teams_dummy.csv'
+    game_path = './data/game.csv'
+    team_path = './data/team_info.csv'
+    game_teams_path = './data/game_teams_stats.csv'
 
 
 
@@ -31,4 +31,22 @@ class LeagueTest < Minitest::Test
   def test_it_returns_total_number_of_teams
     assert_equal 33, @league.count_of_teams
   end
+
+  def test_it_returns_team_name_with_best_offense
+    assert_equal "Golden Knights", @league.best_offense
+  end
+
+  def test_it_returns_team_name_with_worst_offense
+    assert_equal "Sabres", @league.worst_offense
+  end
 end
+
+# best_defense	Name of the team with the lowest average number of goals allowed per game across all seasons.	String
+# worst_defense	Name of the team with the highest average number of goals allowed per game across all seasons.	String
+# highest_scoring_visitor	Name of the team with the highest average score per game across all seasons when they are away.	String
+# highest_scoring_home_team	Name of the team with the highest average score per game across all seasons when they are home.	String
+# lowest_scoring_visitor	Name of the team with the lowest average score per game across all seasons when they are a visitor.	String
+# lowest_scoring_home_team	Name of the team with the lowest average score per game across all seasons when they are at home.	String
+# winningest_team	Name of the team with the highest win percentage across all seasons.	String
+# best_fans	Name of the team with biggest difference between home and away win percentages.	String
+# worst_fans	List of names of all teams with better away records than home records.	Array
