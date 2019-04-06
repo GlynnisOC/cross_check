184d8d7a93213cdbcab6ce252fa4fdc4f50e275b
Ryan Miller <ryanmillergm@gmail.com>
2019-04-06 15:20:39 -0600

    Add least, most accurate and powerplay Methods

--- lib/season.rb
+++ lib/season.rb
@@ -7,7 +7,9 @@ class Season
   end
 
   def biggest_bust(season_id)
-
+    #get team wins & games played for regular season
+    #get team wins & games played for postseason
+    #find percentages and find biggest decrease
 
     # Name of the team with the biggest decrease
     # between regular season and postseason win
@@ -15,30 +17,102 @@ class Season
   end
 
   def biggest_surprise(season_id)
+    #get team wins & games played for regular season
+    #get team wins & games played for postseason
+    #find percentages and find biggest increase
     # Name of the team with the biggest increase
     # between regular season and postseason win
     # percentage.	String
   end
 
   def winningest_coach(season_id)
+    result = {}
+    game_ids = []
 
-    # Name of the Coach with the best win percentage
-    # for the season	String
+    @game_stats.each do |game|
+      game_ids << game[:game_id] if game[:season] == season_id
+    end
+
+      @game_teams_stats.each do |game|
+        if game_ids.include? game[:game_id]
+        result[game[:head_coach]] = {won: 0, total_games: 0} unless result[game[:head_coach]]
+        result[game[:head_coach]][:won] += 1  if game[:won].include?("TRUE")
+        result[game[:head_coach]][:total_games] += 1
+        end
+      end
+      result.max_by do |coach, stats|
+      stats[:won]/stats[:total_games].to_f
+    end.first
   end
 
   def worst_coach(season_id)
-    # Name of the Coach with the worst win percentage
-    # for the season	String
+    result = {}
+    game_ids = []
+
+    @game_stats.each do |game|
+      game_ids << game[:game_id] if game[:season] == season_id
+    end
+
+      @game_teams_stats.each do |game|
+        if game_ids.include? game[:game_id]
+        result[game[:head_coach]] = {won: 0, total_games: 0} unless result[game[:head_coach]]
+        result[game[:head_coach]][:won] += 1  if game[:won].include?("TRUE")
+        result[game[:head_coach]][:total_games] += 1
+        end
+      end
+      result.min_by do |coach, stats|
+      stats[:won]/stats[:total_games].to_f
+    end.first
   end
 
   def most_accurate_team(season_id)
-    # Name of the Team with the best ratio of shots
-    # to goals for the season	String
+    result = {}
+    game_ids = []
+
+    @game_stats.each do |game|
+      game_ids << game[:game_id] if game[:season] == season_id
+    end
+
+      @game_teams_stats.each do |game|
+        if game_ids.include? game[:game_id]
+        result[game[:team_id]] = {shots: 0, goals: 0} unless result[game[:team_id]]
+        result[game[:team_id]][:shots] += game[:shots]
+        result[game[:team_id]][:goals] += game[:goals]
+        end
+      end
+      team_id = result.max_by do |team_id, stats|
+        stats[:goals]/stats[:shots].to_f
+    end
+      team_name = @team_stats.each do |game|
+        if game[:team_id] == team_id.first
+          return game[:teamname]
+        end
+      end
   end
 
   def least_accurate_team(season_id)
-    # Name of the Team with the worst ratio of shots
-    # to goals for the season	String
+    result = {}
+    game_ids = []
+
+    @game_stats.each do |game|
+      game_ids << game[:game_id] if game[:season] == season_id
+    end
+
+      @game_teams_stats.each do |game|
+        if game_ids.include? game[:game_id]
+        result[game[:team_id]] = {shots: 0, goals: 0} unless result[game[:team_id]]
+        result[game[:team_id]][:shots] += game[:shots]
+        result[game[:team_id]][:goals] += game[:goals]
+        end
+      end
+      team_id = result.min_by do |team_id, stats|
+        stats[:goals]/stats[:shots].to_f
+    end
+      team_name = @team_stats.each do |game|
+        if game[:team_id] == team_id.first
+          return game[:teamname]
+        end
+      end
   end
 
   def most_hits(season_id)
@@ -50,8 +124,23 @@ class Season
   end
 
   def power_play_goal_percentage(season_id)
-    # Percentage of goals that were power play goals for the
-    # season (rounded to the nearest 100th)	Float
+    result = {}
+    game_ids = []
+
+    @game_stats.each do |game|
+      game_ids << game[:game_id] if game[:season] == season_id
+    end
+
+      @game_teams_stats.each do |game|
+        if game_ids.include? game[:game_id]
+          # binding.pry
+        result[:total] = {powerplay_goals: 0, goals: 0} unless result[:total]
+        result[:total][:powerplay_goals] += game[:powerplaygoals]
+        result[:total][:goals] += game[:goals]
+        end
+      end
+      (result[:total][:powerplay_goals] / result[:total][:goals].to_f).round(2)
   end
+
 
 end
