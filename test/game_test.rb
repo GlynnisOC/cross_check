46409df7e5a805e35bae1ed3483e71269c05ac1a
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-04-09 15:14:29 -0600

    Refactor game_test for coverage and stat_tracker refactors justin <justin@baal-software.com> ryan <ryanmillergm@gmail.com>

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,75 +1,56 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/game'
-require './lib/stat_tracker'
-require 'pry'
+require './test/test_helper'
 
 class GameTest < Minitest::Test
+  include Util
 
   def setup
-    game_path = './data/dummy_files/game_dummy.csv'
-    team_path = './data/dummy_files/team_dummy.csv'
-    game_teams_path = './data/dummy_files/game_teams_dummy.csv'
-
-
-
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
-
     @stat_tracker = StatTracker.from_csv(locations)
-    @game = @stat_tracker.game
-  end
-
-  def test_it_exists
-    assert_instance_of Game, @game
   end
 
   def test_highest_total_score
-    assert_equal 9, @game.highest_total_score
+    assert_equal 15, @stat_tracker.highest_total_score
   end
 
   def test_lowest_total_score
-    assert_equal 3, @game.lowest_total_score
+    assert_equal 1, @stat_tracker.lowest_total_score
   end
 
   def test_it_returns_biggest_blowout
-    assert_equal 5, @game.biggest_blowout
+    assert_equal 10, @stat_tracker.biggest_blowout
   end
 
   def test_it_returns_percentage_home_wins
-    assert_equal 66.67, @game.percentage_home_wins
+    assert_equal 0.55, @stat_tracker.percentage_home_wins
   end
 
   def test_it_returns_percentage_visitor_wins
-    assert_equal 33.33, @game.percentage_visitor_wins
+    assert_equal 0.45, @stat_tracker.percentage_visitor_wins
   end
 
   def test_count_of_games_by_season
     expected = {
-      20122013=>14,
-      20132014=>4,
-      20142015=>6,
-      20162017=>5,
-      20152016=>1
-    }
+      "20122013"=>806,
+      "20162017"=>1317,
+      "20142015"=>1319,
+      "20152016"=>1321,
+      "20132014"=>1323,
+      "20172018"=>1355}
 
-    assert_equal expected, @game.count_of_games_by_season
+    assert_equal expected, @stat_tracker.count_of_games_by_season
   end
 
   def test_average_goals_per_game
-    assert_equal 5.4, @game.average_goals_per_game
+    assert_equal 5.54, @stat_tracker.average_goals_per_game
   end
 
   def test_average_goals_per_game_per_season
     expected = {
-      20122013=>4.929,
-      20132014=>7.25,
-      20142015=>4.833,
-      20162017=>6.0,
-      20152016=>5.0}
-    assert_equal expected, @game.average_goals_by_season
+      "20122013"=>5.4,
+      "20162017"=>5.51,
+      "20142015"=>5.43,
+      "20152016"=>5.41,
+      "20132014"=>5.5,
+      "20172018"=>5.94}
+    assert_equal expected, @stat_tracker.average_goals_by_season
   end
 end
--- test/season_test.rb
+++ test/season_test.rb
@@ -1,9 +1,4 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/game'
-require './lib/season'
-<<<<<<< Updated upstream
-require './lib/stat_tracker'
+require './test/test_helper'
 
 class SeasonTest < MiniTest::Test
   attr_reader :season
@@ -58,20 +53,7 @@ class SeasonTest < MiniTest::Test
     assert_equal "Sabres", @season.least_accurate_team(20132014)
     assert_equal "Coyotes", @season.least_accurate_team(20142015)
   end
-  
-=======
-require './lib/csv_util'
-require './lib/stat_tracker'
 
-class SeasonTest < Minitest::Test
-  include Util
-  attr_reader :season
-
-  def setup
-    @season = StatTracker.from_csv(locations).season unless season
-  end
-
->>>>>>> Stashed changes
   def test_it_returns_name_of_team_with_most_hits
     assert_equal "Kings", @season.most_hits(20132014)
     assert_equal "Islanders", @season.most_hits(20142015)
@@ -81,12 +63,9 @@ class SeasonTest < Minitest::Test
     assert_equal "Devils", @season.fewest_hits(20132014)
     assert_equal "Wild", @season.fewest_hits(20142015)
   end
-<<<<<<< Updated upstream
-  
+
   def test_power_play_goal_percentage
     assert_equal 0.22, @season.power_play_goal_percentage(20132014)
     assert_equal 0.21, @season.power_play_goal_percentage(20142015)
   end
-=======
->>>>>>> Stashed changes
 end
